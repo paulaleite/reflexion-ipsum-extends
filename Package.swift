ea4f05b14ec87136bbfcb00bff736e9998161501
Paula Leite <paula.t.leite@icloud.com>
2020-04-07 17:11:37 -0300

    added dependecies

--- Package.swift
+++ Package.swift
@@ -12,15 +12,14 @@ let package = Package(
             targets: ["reflexion-ipsum-extends"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/JulioBBL/reflectium-ipsum", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "reflexion-ipsum-extends",
-            dependencies: []),
+            dependencies: ["reflectium-ipsum"]),
         .testTarget(
             name: "reflexion-ipsum-extendsTests",
             dependencies: ["reflexion-ipsum-extends"]),
