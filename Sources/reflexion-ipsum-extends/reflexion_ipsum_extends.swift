d4c31276db18d08846c4b42be778a52805c5c49d
Paula Leite <paula.t.leite@icloud.com>
2020-04-07 17:26:16 -0300

    added functions

--- Sources/reflexion-ipsum-extends/reflexion_ipsum_extends.swift
+++ Sources/reflexion-ipsum-extends/reflexion_ipsum_extends.swift
@@ -1,3 +1,14 @@
-struct reflexion_ipsum_extends {
-    var text = "Hello, World!"
+import UIKit
+import reflectium_ipsum
+
+public final class FinalReflexionGenerator {\
+    
+    public static func generateUppercaseFeedback() -> String {
+        return ReflectionGenerator.generatiumReflectium().uppercased()
+    }
+    
+    public static func generatePaulasFeedback() -> String {
+        return ReflectionGenerator.generatiumReflectium().append(contentsOf: "Uhhh, a Paula passou aqui.")
+    }\
+    
 }
